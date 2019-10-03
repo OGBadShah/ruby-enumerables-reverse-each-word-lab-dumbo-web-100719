c095fceec5cd66888eab222d483a30d97cc65ef2
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-03 03:26:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,17 @@
 def reverse_each_word(string)
-  split_array = string.split(" ")
+  array = string.split(" ")
   new_array = []
-  split_array.each do |string|
+  array.each do |string|
     new_array.push(string.reverse)
   end
   new_array.join(" ")
 end
 
 def reverse_each_word(string)
-  
+  array2 = string.split(" ")
+  new_array2 = []
+  array.collect do |string|
+    new_array2.push(string.reverse)
+  end
+  new_array2.join(" ")
+end
