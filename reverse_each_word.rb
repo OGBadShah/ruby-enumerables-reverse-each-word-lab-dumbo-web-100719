269851ee398cbb1f7794ca993cbc9ba74751a26e
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-03 03:41:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,10 +10,10 @@ end
 
 
 def reverse_each_word(string)
-  array2 = string.split(" ")
-  new_array2 = []
-  array.collect do |words|
-    new_array2.push(words.reverse)
+  array1 = string.split(" ")
+  new_array = []
+  array1.collect do |word|
+    new_array.push(word.reverse)
   end
-  new_array2.join(" ")
+  new_array.join(" ")
 end
